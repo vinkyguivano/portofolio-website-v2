0fc67a3baa76300f5a2c6d25eaef3741f6e7db84
vinkyguivano <vinkyguivano@gmail.com>
2025-05-06 00:06:36 +0700

    feat: add logo

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+
 const Header = () => {
   return (
     <>
       <div className="fixed inset-x-0 flex items-center justify-between py-[35px] px-[150px] font-poppins bg-black">
-        <div>Logo</div>
+        <div>
+          <Image
+            src="/Logo VG.png"
+            alt="logo VG"
+            width={40}
+            height={40}
+          />
+        </div>
         <div className="grid grid-cols-2 gap-[10px] text-center">
           <button className="border-2 border-gray-600 rounded-xl py-2 px-3.5 font-medium text-[13px] text-[#E1E1E1] cursor-pointer hover:bg-gray-900">
             About Me
