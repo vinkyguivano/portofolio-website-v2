13f1189265d4a941d66febf9d1793d2fe4a47cef
vinkyguivano <vinkyguivano@gmail.com>
2025-05-05 23:52:39 +0700

    feat: add hover variant styling

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,13 +1,13 @@
 const Header = () => {
   return (
     <>
-      <div className="fixed inset-x-0 flex items-center justify-between py-[35px] px-[150px] font-poppins">
+      <div className="fixed inset-x-0 flex items-center justify-between py-[35px] px-[150px] font-poppins bg-black">
         <div>Logo</div>
         <div className="grid grid-cols-2 gap-[10px] text-center">
-          <button className="border-2 border-gray-600 rounded-xl py-2 px-3.5 font-medium text-[13px] text-[#E1E1E1]">
+          <button className="border-2 border-gray-600 rounded-xl py-2 px-3.5 font-medium text-[13px] text-[#E1E1E1] cursor-pointer hover:bg-gray-900">
             About Me
           </button>
-          <button className="bg-white rounded-xl py-2 px-3.5 font-medium text-[13px] text-black">
+          <button className="bg-white rounded-xl py-2 px-3.5 font-medium text-[13px] text-black cursor-pointer hover:bg-gray-200">
             Contact
           </button>
         </div> 
