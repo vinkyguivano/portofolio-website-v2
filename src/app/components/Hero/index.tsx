b1f94bf79033f2b1149d63480d141b2572a34823
vinkyguivano <vinkyguivano@gmail.com>
2025-05-06 12:56:09 +0700

    feat: add title

--- src/app/components/Hero/index.tsx
+++ src/app/components/Hero/index.tsx
@@ -10,6 +10,13 @@ const Hero = () => {
         height={230}
         className="rounded-full bg-[#1A1A1A] mx-auto w-60 h-60 object-center object-contain"
       />
+      <div className="mt-9 font-poppins">
+        <div className="font-bold text-6xl">Vinky Guivano!</div>
+        <div className="font-semibold text-[#878787] text-3xl">
+          I do Code &
+          <span className="bg-gradient-to-r from-purple-400 via-pink-500 to-orange-400 bg-clip-text text-transparent"> Chill</span>
+        </div>
+      </div>
     </div>
   )
 }
