3afc34f0e1e2b1a2ad4f01dcfa7f3f0cbbdac48e
vinkyguivano <vinkyguivano@gmail.com>
2025-05-09 21:11:58 +0700

    chore: update button variant

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -14,10 +14,10 @@ const Header = () => {
             />
           </div>
           <div className="grid grid-cols-2 gap-[10px]">
-            <button className="border-2 border-gray-600 rounded-xl py-2 px-3.5 font-medium text-[13px] text-[#E1E1E1] cursor-pointer hover:bg-gray-900">
+            <button className="border-2 border-gray-600 rounded-xl py-2 px-3.5 font-medium text-[13px] text-[#E1E1E1] cursor-pointer">
               About Me
             </button>
-            <button className="bg-white rounded-xl py-2 px-3.5 font-medium text-[13px] text-black cursor-pointer hover:bg-gray-200">
+            <button className="bg-white rounded-xl py-2 px-3.5 font-medium text-[13px] text-black cursor-pointer">
               Contact
             </button>
           </div> 
