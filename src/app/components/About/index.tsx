757ef934ca6521b708d77eabdbb9a7088e48353e
vinkyguivano <vinkyguivano@gmail.com>
2025-05-16 21:05:17 +0700

    feat: add about me section

---
+++ src/app/components/About/index.tsx
@@ -0,0 +1,18 @@
+const AboutMe = () => {
+  return (
+    <div className="mt-[105px] font-inter">
+      <div className="font-extrabold text-5xl">ABOUT ME</div>
+      <div className="font-medium text-[11px] bg-[linear-gradient(to_right,_var(--color-purple-400)_20%,_var(--color-orange-400)_100%)] bg-clip-text text-transparent tracking-[7px]">EXPLORE NOW</div>
+      <div className="mt-[27px]">
+        Hi Peeps &#128075;, I&apos;m Vinky Guivano, a Web Software Engineer with 3 years of experience building responsive, user-focused websites and applications.
+        I enjoy turning complex ideas into clean, functional codes that enhance seamlessness of user experience.
+        <br/>
+        <br/>
+        Outside of coding, I&apos;m passionate about playing football and hitting the gym. Staying active helps me stay focused, energized, and always ready to tackle
+        the next challenge, wheter on the field or in code. 
+      </div>
+    </div>
+  )
+}
+
+export default AboutMe
