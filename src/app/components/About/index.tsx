db4a69f5a2561c786b77c06a96c5946b12021896
vinkyguivano <vinkyguivano@gmail.com>
2025-05-16 21:15:13 +0700

    feat: add icon

--- src/app/components/About/index.tsx
+++ src/app/components/About/index.tsx
@@ -4,11 +4,11 @@ const AboutMe = () => {
       <div className="font-extrabold text-5xl">ABOUT ME</div>
       <div className="font-medium text-[11px] bg-[linear-gradient(to_right,_var(--color-purple-400)_20%,_var(--color-orange-400)_100%)] bg-clip-text text-transparent tracking-[7px]">EXPLORE NOW</div>
       <div className="mt-[27px]">
-        Hi Peeps &#128075;, I&apos;m Vinky Guivano, a Web Software Engineer with 3 years of experience building responsive, user-focused websites and applications.
+        Hi Peeps &#128075;, I&apos;m Vinky Guivano, a Web Software Engineer &#128187; with 3 years of experience building responsive, user-focused websites and applications.
         I enjoy turning complex ideas into clean, functional codes that enhance seamlessness of user experience.
         <br/>
         <br/>
-        Outside of coding, I&apos;m passionate about playing football and hitting the gym. Staying active helps me stay focused, energized, and always ready to tackle
+        Outside of coding, I&apos;m passionate about playing football &#9917; and hitting the gym &#x1F3CB;. Staying active helps me stay focused, energized, and always ready to tackle
         the next challenge, whether on the field or in code. 
       </div>
     </div>
