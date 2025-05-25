88a657497b23a4c570d21e58bcf37af98333d51e
vinkyguivano <vinkyguivano@gmail.com>
2025-05-25 23:09:59 +0700

    feat: scroll into HTML

--- src/app/context/LanguageContext.tsx
+++ src/app/context/LanguageContext.tsx
@@ -10,7 +10,7 @@ export const LanguageProvider = ({ children }: { children: React.ReactNode}) => 
 
   const toggleLanguage = useCallback(() => {
     setLanguage(prev => (prev === "EN" ? "ID" : "EN"));
-    document.querySelector("body")?.scrollIntoView({ behavior: 'smooth' })
+    document.querySelector("html")?.scrollIntoView({ behavior: 'smooth' })
   }, [])
 
   const t = useCallback((key: string) => {
